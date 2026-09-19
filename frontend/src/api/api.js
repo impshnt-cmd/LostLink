0d2e15633dd800a21524ee459c8a6de4e71bcb37
parro student <ps9224933@gmail.com>
2026-09-19 23:26:23 +0530

    Update frontend API URL for deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,10 @@
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
 });
 
 API.interceptors.request.use(
@@ -30,3 +33,4 @@ API.interceptors.request.use(
 );
 
 export default API;
+
